d6a6031de5da3cb70063481206b32367d7c4d42b
Ben Robey <python08@gmail.com>
2017-03-04 14:43:19 +0000

    init

---
+++ src/Tests/VehicleTest.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class VehicleTest extends TestCase
+{
+    public function testNamespacing() {
+        $vehicle = new Cazana\Vehicle();
+    }
+
+}
+?>
+
---
+++ src/Vehicle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ben
+ * Date: 4/3/17
+ * Time: 2:31 PM
+ */
+
+namespace Cazana;
+
+
+class Vehicle
+{
+
+
+}
